eb5f1635895b241054f87df8cf11ba4667fea6cd
lonegunman <lonegunmanb@hotmail.com>
2019-07-02 18:40:37 +0800

    enable docker service

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,4 +1,5 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli
+systemctl enable docker
 sync
