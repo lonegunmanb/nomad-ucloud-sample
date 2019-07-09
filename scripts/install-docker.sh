96addb5c4d0cf59106d2955b70c8ac844d049e18
lonegunman <lonegunmanb@hotmail.com>
2019-07-09 17:50:33 +0800

    Revert "change docker yum repo to aliyun"
    This reverts commit 8cc518f5

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,5 +1,5 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli
 systemctl enable docker
 sync
