bd5ffe24d9222725853af1d79e6db7ad31c2f55b
lonegunman <lonegunmanb@hotmail.com>
2019-07-15 23:06:30 +0800

    rename

--- consul-server/outputs.tf
+++ consul-server/outputs.tf
@@ -2,6 +2,6 @@ output "public_ips" {
   value = "${ucloud_eip.consul_servers.*.public_ip}"
 }
 
-output "consul_server_private_ips" {
+output "private_ips" {
   value = "${ucloud_instance.consul_server.*.private_ip}"
 }
