1f43703ad85ec1301bd16a8e14951f555fb10702
lonegunman <lonegunmanb@hotmail.com>
2019-07-17 15:55:51 +0800

    split nomad_client_type to nomad_client_namesvr_type and nomad_client_broker_type

--- variables.tf
+++ variables.tf
@@ -1,48 +1,42 @@
 variable "consul_server_root_password" {
-  default = ""
 }
 variable "nomad_server_root_password" {
-  default = ""
 }
 variable "nomad_client_root_password" {
-  default = ""
 }
 variable ucloud_pub_key {
-  default = ""
 }
 variable ucloud_secret {
-  default = ""
 }
 variable region {
   default = "cn-bj2"
 }
 variable az {
   default = [
+    "cn-bj2-02",
     "cn-bj2-03",
-    "cn-bj2-04",
-    "cn-bj2-05"]
+    "cn-bj2-04"]
 }
 variable project_id {
-  default = ""
 }
 variable consul_server_type {
-  default = "n-standard-1"
+  default = "n-highcpu-1"
 }
 variable nomad_server_type {
-  default = "n-standard-1"
+  default = "n-highcpu-1"
 }
-variable "nomad_client_type" {
-  default = "n-standard-1"
+variable nomad_client_namesvr_type {
+  default = "n-highmem-1"
+}
+variable nomad_client_broker_type {
+  default = "n-highmem-1"
 }
 variable allow_ip {
   default = "0.0.0.0/0"
 }
 variable consul_server_image_id {
-  default = ""
 }
 variable nomad_server_image_id {
-  default = ""
 }
 variable nomad_client_image_id {
-  default = ""
 }
