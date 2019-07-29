b137565efd35791cf42908fd6bba368c6131fc7a
lonegunman <lonegunmanb@hotmail.com>
2019-07-29 16:56:28 +0800

    ...

--- network/variables.tf
+++ network/variables.tf
@@ -1,20 +1,8 @@
 variable az {
   default = "cn-bj2-02"
 }
-variable ucloud_pub_key {
-  default = "mBpLavCXClh7vtW5hrzKA2/LcTm+JnmCHmItMpm3Z4hGxUuFmuMuDw4myeNn31nz"
-}
-variable region {
-  default = "cn-bj2"
-}
-variable ucloud_secret {
-  default = "nUPm/5epJeT1ogHIVwYnmWvP0YP2opetFVDiw2QxfMNMWGgj9RGgKKjmQpH4VOPm"
-}
-
-variable project_id {
-  default = "org-ttq14t"
-}
-
-variable cluster_id {
-  default = "rogerhe"
-}
+variable ucloud_pub_key {}
+variable region {}
+variable ucloud_secret {}
+variable project_id {}
+variable cluster_id {}
