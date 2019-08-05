ef2d0776521a83968e214cd2ccb0840cfbf672e8
lonegunman <lonegunmanb@hotmail.com>
2019-08-05 18:15:09 +0800

    pass plugin version via variables

--- scripts/build-terraform-plugin-cache.sh
+++ scripts/build-terraform-plugin-cache.sh
@@ -1,11 +1,15 @@
 mkdir /plugin
 cd /plugin
-wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-consul_v2.5.0_x4
-wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-null_v2.1.2_x4
-wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-template_v2.1.2_x4
-wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-ucloud_v1.11.1_x4
-chmod +x terraform-provider-consul_v2.5.0_x4
-chmod +x terraform-provider-null_v2.1.2_x4
-chmod +x terraform-provider-template_v2.1.2_x4
-chmod +x terraform-provider-ucloud_v1.11.1_x4
+TF_PLUGIN_CONSUL_VERSION = $TF_PLUGIN_CONSUL_VERSION
+TF_PLUGIN_NULL_VERSION = $TF_PLUGIN_NULL_VERSION
+TF_PLUGIN_TEMPLATE_VERSION = $TF_PLUGIN_TEMPLATE_VERSION
+TF_PLUGIN_UCLOUD_VERSION = $TF_PLUGIN_UCLOUD_VERSION
+wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-consul_v${TF_PLUGIN_CONSUL_VERSION}_x4
+wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-null_v${TF_PLUGIN_NULL_VERSION}_x4
+wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-template_v${TF_PLUGIN_TEMPLATE_VERSION}_x4
+wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-ucloud_v${TF_PLUGIN_UCLOUD_VERSION}_x4
+chmod +x terraform-provider-consul_v${TF_PLUGIN_CONSUL_VERSION}_x4
+chmod +x terraform-provider-null_v${TF_PLUGIN_NULL_VERSION}_x4
+chmod +x terraform-provider-template_v${TF_PLUGIN_TEMPLATE_VERSION}_x4
+chmod +x terraform-provider-ucloud_v${TF_PLUGIN_UCLOUD_VERSION}_x4
 sync
