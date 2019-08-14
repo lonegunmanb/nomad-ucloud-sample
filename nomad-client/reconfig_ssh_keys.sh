f14d04583d82e244b24ba92bcf4ca735c4cd1d27
lonegunman <lonegunmanb@hotmail.com>
2019-08-14 14:52:08 +0800

    ensure reconfig ssh won't been blocked

--- nomad-client/reconfig_ssh_keys.sh
+++ nomad-client/reconfig_ssh_keys.sh
@@ -1,4 +1,4 @@
 rm -f /etc/ssh/ssh_host_*
-ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key
-ssh-keygen -q -N "" -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key
-ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+echo -e 'y\n' | ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key
+echo -e 'y\n' | ssh-keygen -q -N "" -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key
+echo -e 'y\n' | ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
