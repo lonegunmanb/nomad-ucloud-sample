ac7246a97b633ba3f4dc3896f50e2fc03ffd235f
lonegunman <lonegunmanb@hotmail.com>
2019-08-16 16:54:19 +0800

    add terraform nomad plugin to image

--- scripts/build-terraform-plugin-cache.sh
+++ scripts/build-terraform-plugin-cache.sh
@@ -1,11 +1,13 @@
 mkdir /plugin
 cd /plugin
 wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-consul_v${TF_PLUGIN_CONSUL_VERSION}_x4
+wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-nomad_v${TF_PLUGIN_NOMAD_VERSION}_x4
 wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-null_v${TF_PLUGIN_NULL_VERSION}_x4
 wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-template_v${TF_PLUGIN_TEMPLATE_VERSION}_x4
 wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-ucloud_v${TF_PLUGIN_UCLOUD_VERSION}_x4
 wget -nv http://hashicorpfile.cn-bj.ufileos.com/terraform-provider-external_v${TF_PLUGIN_EXTERNAL_VERSION}_x4
 chmod +x terraform-provider-consul_v${TF_PLUGIN_CONSUL_VERSION}_x4
+chmod +x terraform-provider-consul_v${TF_PLUGIN_NOMAD_VERSION}_x4
 chmod +x terraform-provider-null_v${TF_PLUGIN_NULL_VERSION}_x4
 chmod +x terraform-provider-template_v${TF_PLUGIN_TEMPLATE_VERSION}_x4
 chmod +x terraform-provider-ucloud_v${TF_PLUGIN_UCLOUD_VERSION}_x4
