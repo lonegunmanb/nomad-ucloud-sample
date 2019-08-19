f60ae5c149c2e69fa157c140614ed51e0e88e486
lonegunman <lonegunmanb@hotmail.com>
2019-08-19 22:27:35 +0800

    use centos instead of alpine for bootstrapper because we need consul and nomad to run acl bootstrap, and nomad cannot run on alpine for now

--- scripts/config-apk.sh
+++ scripts/config-apk.sh
@@ -1,1 +1,4 @@
 echo https://mirrors.cloud.tencent.com/alpine/latest-stable/main/ > /etc/apk/repositories
+echo https://mirrors.aliyun.com/alpine/latest-stable/main/ >> /etc/apk/repositories
+echo https://mirrors.ustc.edu.cn/alpine/latest-stable/main >> /etc/apk/repositories
+echo https://mirrors.ustc.edu.cn/alpine/latest-stable/community >> /etc/apk/repositories
--- scripts/install-bootstrapper.sh
+++ scripts/install-bootstrapper.sh
@@ -1,10 +1,20 @@
-apk add curl wget git python2 jq
+yum install -y curl wget git python2 jq unzip
 cd /tmp
+
+wget  -nv -O nomad.zip http://hashicorpfile.cn-bj.ufileos.com/nomad_${NOMAD_VERSION}_linux_amd64.zip
+unzip nomad.zip
+install nomad /usr/local/bin/nomad
+nomad version
 
 wget -nv -O terraform.zip http://hashicorpfile.cn-bj.ufileos.com/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform
 install terraform /usr/local/bin/terraform
 terraform version
+
+wget -nv -O consul.zip http://hashicorpfile.cn-bj.ufileos.com/consul_${CONSUL_VERSION}_linux_amd64.zip
+unzip consul.zip
+install consul /usr/local/bin/consul
+consul --version
 
 rm -rf /tmp/*
 sync
