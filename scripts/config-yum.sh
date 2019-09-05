57c04779f51be4901eb268249df8b06233f31512
lonegunman <lonegunmanb@hotmail.com>
2019-09-05 13:10:23 +0800

    update terraform version

--- scripts/config-yum.sh
+++ scripts/config-yum.sh
@@ -1,9 +1,9 @@
 echo "add yum repo"
-yum install -y wget
 rm -f /etc/yum.repos.d/CentOS-Base.repo
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
+curl http://mirrors.163.com/.help/CentOS7-Base-163.repo -o /etc/yum.repos.d/CentOS-Base.repo
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache
+yum install -y wget
 echo "yum upgrade"
 yum upgrade -y
 yum install -y yum-utils device-mapper-persistent-data lvm2
