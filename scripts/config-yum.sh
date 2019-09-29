ad49281401bc3836df228fee7787e395c541bcdd
lonegunman <lonegunmanb@hotmail.com>
2019-09-29 10:14:42 +0800

    install-docker support install from an rpm

--- scripts/config-yum.sh
+++ scripts/config-yum.sh
@@ -1,8 +1,9 @@
 echo "add yum repo"
 sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/fastestmirror.conf
-rm -f /etc/yum.repos.d/CentOS-Base.repo
-curl ${YUM_BASE} -o /etc/yum.repos.d/CentOS-Base.repo
-
+if [[ ! -z "${YUM_YUM_BASE}" ]]; then
+  rm -f /etc/yum.repos.d/CentOS-Base.repo
+  curl ${YUM_BASE} -o /etc/yum.repos.d/CentOS-Base.repo
+fi
 if [[ ! -z "${YUM_DOCKER}" ]]; then
   yum-config-manager --add-repo ${YUM_DOCKER}
 fi
--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,4 +1,19 @@
 echo "install docker-ce"
-yum install -y docker-ce docker-ce-cli
+if [[ ! -z "${DOCKER_RPM}" ]]; then
+  wget -nv -O docker-ce.rpm ${DOCKER_RPM}
+  yum install -y docker-ce.rpm
+  rm -f docker-ce.rpm
+else
+  yum install -y docker-ce
+fi
+
+if [[ ! -z "${DOCKER_CLI_RPM}" ]]; then
+  wget -nv -O docker-cli.rpm ${DOCKER_CLI_RPM}
+  yum install -y docker-cli.rpm
+  rm -f docker-cli.rpm
+else
+  yum install -y docker-ce-cli
+fi
+
 systemctl enable docker
 sync
