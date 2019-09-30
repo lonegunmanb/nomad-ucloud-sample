c1ca312aeb7cf29c3e76ca0e6db1fedf346a2f8c
lonegunman <lonegunmanb@hotmail.com>
2019-09-30 14:14:55 +0800

    because we use file to pass resource id, terraform will delete uhost before delete attachment sometimes, it'll cause error on destroy

--- internal_lb/outputs.tf
+++ internal_lb/outputs.tf
@@ -1,9 +1,9 @@
 output "lb_ip" {
-  value = var.legacy_lb_private_ip == "" ? ucloud_lb.lb.*.private_ip[0] : var.legacy_lb_private_ip
+  value = var.legacy_lb_private_ip == "" ? (length(ucloud_lb.lb) == 0 ? "" :ucloud_lb.lb.*.private_ip[0]) : var.legacy_lb_private_ip
 }
 
 output "lb_id" {
-  value = var.legacy_lb_id == "" ? ucloud_lb.lb.*.id[0] : var.legacy_lb_id
+  value = var.legacy_lb_id == "" ? (length(ucloud_lb.lb) == 0 ? "" : ucloud_lb.lb.*.id[0]) : var.legacy_lb_id
 }
 
 output "setup_loopback_script" {
